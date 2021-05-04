e8b7b2b49b47d5c08928b7b45a2eee27f331e1d6
SoloChristo <acsmith@ucsd.edu>
2021-05-03 21:53:59 -0700

    clear text boxs when user clicks clear key.

--- script.js
+++ script.js
@@ -22,17 +22,7 @@ photo_input.addEventListener('change', () => {
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
   // TODO
-  // let canvas = document.getElementById("user-image");
-  // // let ctx = canvas.getContext("2d");
-  // if (canvas.getContext) {
-  //   let ctx = canvas.getContext('2d');
-  //   console.log("lmao");
-  //   ctx.fillStyle = 'rgb(200, 0, 0)';
-  //   ctx.fillRect(10, 10, 50, 50);
 
-  //   ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-  //   ctx.fillRect(30, 30, 50, 50);
-  // }
   //Taking care of the canvas
   //const canvas = document.getElementById('user-image');
   wipeCanvas();
@@ -63,6 +53,9 @@ submit.addEventListener('click', (e) => {
 clear.addEventListener('click', (e) => {
   e.preventDefault();
   ctx.clearRect(0,0, canvas.width, canvas.height);
+  document.getElementById('text-top').value = "";
+  document.getElementById('text-bottom').value = "";
+
   enableButtonsClear();
 });
 
